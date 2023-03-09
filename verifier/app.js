464e823750d6ff81d4470fee1f1ec8bb82da6b91
txhsl <799498265@qq.com>
2023-03-09 18:29:52 +0800

    add https server

--- verifier/app.js
+++ verifier/app.js
@@ -1,11 +1,17 @@
 import { rpc } from "@cityofzion/neon-js";
-import express from 'express';
+import express from "express";
+import fs from "fs";
+import https from "https";
 import cors from "cors";
 import md5 from "js-md5";
 import url from "url";
 import { request } from "http";
 
-const app = express()
+const options = {
+    key: fs.readFileSync('./server.key'),
+    cert: fs.readFileSync('./server.crt')
+}
+const app = express();
 app.use(express.json());
 app.use(cors());
 
@@ -25,8 +31,12 @@ const deviceName = '4GC30223';
 const coinCount = 1;
 
 app.listen(8080, () => {
-    console.log('Express server running at http://127.0.0.1');
+    console.log('Http server running at http://127.0.0.1:8080');
 })
+
+https.createServer(options, app).listen(8443, () => {
+    console.log('Https server running at https://127.0.0.1:8443');
+});
 
 app.get('/', (req, res) => {
     res.send('Verifier is working');
@@ -115,7 +125,6 @@ const requestGacha = (id) => {
             });
         });
         req.setTimeout(6000);
-        console.log(JSON.stringify(postData));
         req.write(JSON.stringify(postData), 'utf8');
         req.end();
     });
